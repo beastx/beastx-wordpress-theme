2a5c5be440410cf86fbda3d8aec80f8f0f5e291f
Leandro Asrilevich <beastx@gmail.com>
2009-01-08 23:34:10 -0200

    added sifr 3.0 files

---
+++ sifr/sifr-config.js
@@ -0,0 +1,59 @@
+var SohoGothicProLight = { src: '/wp-content/themes/beastx/sifr/SohoGothicProLight.swf' };
+var SohoGothicProMedium = { src: '/wp-content/themes/beastx/sifr/SohoGothicProMedium.swf' };
+var SohoGothicProMediumItalic = { src: '/wp-content/themes/beastx/sifr/SohoGothicProMediumItalic.swf' };
+
+sIFR.useStyleCheck = true;
+sIFR.activate(SohoGothicProLight, SohoGothicProMedium, SohoGothicProMediumItalic);
+
+sIFR.replace(SohoGothicProMedium, {
+    selector: '.entryTitle h1',
+    css: [
+        '.sIFR-root { background:transparent; color: #8AC11B; z-index: -10 }',
+        'a { text-decoration: none; color:#8AC11B }',
+        'a:link { color: #8AC11B; }',
+        'a:hover { color: #8AC11B; }'
+    ],
+    wmode: 'transparent',
+    filters: {
+        DropShadow: {
+            distance: 0.3,
+            color: '#333333',
+            strength: 2
+        }
+    }
+});
+
+sIFR.replace(SohoGothicProMedium, {
+    selector: '.entryTitle h2',
+    css: [
+        '.sIFR-root {background:transparent; color: #8AC11B; z-index: -10 }',
+        'a { text-decoration: none; color:#8AC11B }',
+        'a:link { color: #8AC11B; }',
+        'a:hover { color: #8AC11B; }'
+    ],
+    wmode: 'transparent',
+    filters: {
+        DropShadow: {
+            distance: 0.3,
+            color: '#333333',
+            strength: 2
+        }
+    }
+});
+
+
+sIFR.replace(SohoGothicProLight, {
+    selector: '.mainButtonsList .mainButton',
+    css: [
+        '.sIFR-root { background-color: #2D183F; color: #f9f9f9; }',
+        'a { text-decoration: none; font-size: 12em; color:#eeeeee }',
+        'a:link { color: #eeeeee; }',
+        'a:hover { color: #ffffff; }'
+    ]
+});
+
+
+sIFR.replace(SohoGothicProMediumItalic, {
+    selector: 'h2.blogDescription',
+    css: '.sIFR-root { background-color: #261138; color: #4E4262; font-style: italic; }'
+});
