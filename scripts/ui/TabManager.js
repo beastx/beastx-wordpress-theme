794071e5c8937b6fc936bef9760c9224e1705dc6
Leandro Asrilevich <leandro@avature.net>
2009-01-10 09:29:51 -0200

    port to prototype tabManager widget

--- scripts/ui/TabManager.js
+++ scripts/ui/TabManager.js
@@ -4,32 +4,30 @@ var TabManager = function() {
 TabManager.prototype.init = function(containerElemnetId, tabLabelsElementTagName, initialSelectedTab) {
     this.labels = [];
     this.contents = [];
-    this.containerElement = DOM.get(containerElemnetId);
+    this.containerElement = $(containerElemnetId);
     this.tabLabelsElementTagName = tabLabelsElementTagName;
     this.selectedTab = initialSelectedTab ? initialSelectedTab : 0;
-    this.eventManager = new EVT.EventManager();
-    this.eventManager.init(this);
-    this.eventManager.addListener('select', EVT.createCaller(this, 'onTabLabelClick'));
     this.prepareTabsElements();
     this.updateUI();
 }
 
 TabManager.prototype.prepareTabsElements = function() {
-    var tempLabelsElements = DOM.getElementsBySelector(this.tabLabelsElementTagName, this.containerElement);
+    var tempLabelsElements = this.containerElement.select(this.tabLabelsElementTagName);
     for (var i = 0; i < tempLabelsElements.length; ++i) {
         var contentElements = this.getContentBeetwenTabLabels(tempLabelsElements[i], tempLabelsElements[i + 1]);
-        DOM.removeChild(this.containerElement, tempLabelsElements[i]);
+        this.containerElement.removeChild(tempLabelsElements[i]);
         
-        var label = new TabLabel();
-        label.init(tempLabelsElements[i].firstChild.nodeValue, i, (i == this.selectedTab), this.eventManager, tempLabelsElements.length);
+        var label = New(TabLabel, [ tempLabelsElements[i].firstChild.nodeValue, i, (i == this.selectedTab), this.eventManager, tempLabelsElements.length ]);
         this.labels.push(label);
         
-        var content = new TabContent();
-        content.init(contentElements, i, (i == this.selectedTab), this.eventManager);
+        var content = New(TabContent,  [ contentElements, i, (i == this.selectedTab), this.eventManager ]);
         this.contents.push(content);
     }
     
-    DOM.removeAllChildren(this.containerElement);
+    while(this.containerElement.childNodes.length > 0) {
+        this.containerElement.removeChild(this.containerElement.firstChild);
+    }
+    document.observe("tabManager:tabselect", createCaller(this, 'onTabLabelClick'));
 }
 
 TabManager.prototype.getContentBeetwenTabLabels = function(tab1, tab2) {
@@ -39,35 +37,41 @@ TabManager.prototype.getContentBeetwenTabLabels = function(tab1, tab2) {
     }
     if (tab1 && tab2) {
         currentElement = tab1;
-        while (DOM.getNextElement(currentElement) != tab2) {
-            elements.push(DOM.getNextElement(currentElement));
-            currentElement = DOM.getNextElement(currentElement);
+        while (currentElement.next(0) != tab2) {
+            elements.push(currentElement.next(0));
+            currentElement = currentElement.next(0);
         }
     } else {
         currentElement = tab1;
-        while (DOM.getNextElement(currentElement)) {
-            elements.push(DOM.getNextElement(currentElement));
-            currentElement = DOM.getNextElement(currentElement);
+        while (currentElement.next(0)) {
+            elements.push(currentElement.next(0));
+            currentElement = currentElement.next(0);
         }
     }
     return elements;
 }
     
 TabManager.prototype.updateUI = function() {
-    var labelsElements = [DOM.createElement('div', { 'class': 'leftCorner' }, [ '' ])];
-    var contentsElements = [];
+    this.labelsContainer = new Element('div', { 'class': 'tabLabels' });
+    this.contentsContainer = new Element('div', { 'class': 'tabContents' });
+    
+    this.labelsContainer.insert(new Element('div', { 'class': 'leftCorner' }).insert(''));
+    
     for (var i = 0; i < this.labels.length; ++i) {
-        labelsElements.push(this.labels[i].element);
+        this.labelsContainer.insert(this.labels[i].element);
     }
-    labelsElements.push(DOM.createElement('div', { 'class': 'rightCorner' }, [ '' ]));
+    this.labelsContainer.insert(new Element('div', { 'class': 'rightCorner' }).insert(''));
+    
     for (var i = 0; i < this.contents.length; ++i) {
-        contentsElements.push(this.contents[i].element);
+        this.contentsContainer.insert(this.contents[i].element);
     }
-    this.element = DOM.createElement('div', { 'class': 'tabManager' }, [
-        this.labelsContainer = DOM.createElement('div', { 'class': 'tabLabels' }, labelsElements),
-        this.contentsContainer = DOM.createElement('div', { 'class': 'tabContents' }, contentsElements)
-    ]);
-    DOM.appendChild(this.containerElement, this.element);
+    
+    this.element = new Element('div', { 'class': 'tabManager' });
+    
+    this.element.insert(this.labelsContainer);
+    this.element.insert(this.contentsContainer);
+    
+    this.containerElement.insert(this.element);
     this.selectTab(this.selectedTab);
 }
 
@@ -83,8 +87,8 @@ TabManager.prototype.unSelectTab = function(index) {
     this.contents[index].setSelected(false);
 }
 
-TabManager.prototype.onTabLabelClick = function(eventObj) {
-    this.selectTab(eventObj.index);
+TabManager.prototype.onTabLabelClick = function(event) {
+    this.selectTab(event.memo.index);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -102,29 +106,31 @@ TabLabel.prototype.init = function(text, index, selected, parentEventManager, ta
 }
 
 TabLabel.prototype.updateUI = function() {
-    this.element = DOM.createElement('div',
-        { 'class': 'tabLabel' + (this.index == 0 ? ' firstTab' : (this.index == this.tabsLength -1 ? ' lastTab' : ''))},
-        [
-            DOM.createElement('a', 
-                {
-                    href: '#',
-                    onclick: EVT.createCaller(this, 'onLabelClick')
-                },
-                [ this.text ]
-            )
-        ]
+    this.element = new Element('div',
+        { 'class': 'tabLabel' + (this.index == 0 ? ' firstTab' : (this.index == this.tabsLength -1 ? ' lastTab' : ''))}
     );
+    this.element.insert(
+        new Element('a', 
+            {
+                href: '#'
+            }
+        ).insert( this.text )
+    );
+    Event.observe(this.element, 'click', createCaller(this, 'onLabelClick'));
 }
 
-TabLabel.prototype.onLabelClick = function(event) {
-    EVT.cancelEvent(event);
-    this.eventManager.dispatchEvent('select', { index: this.index });
-    //~ Log(this.index);
+TabLabel.prototype.onLabelClick = function(event, aaa) {
+    this.element.fire('tabManager:tabselect', { index: this.index });
+    event.stop();
 }
 
 TabLabel.prototype.setSelected = function(selected) {
     this.selected = selected;
-    DOM.setHasClass(this.element, 'selected', this.selected);
+    if (selected) {
+        this.element.addClassName('selected');
+    } else {
+        this.element.removeClassName('selected');
+    }
 }
 
 
@@ -143,10 +149,17 @@ TabContent.prototype.init = function(contentElements, index, selected, parentEve
 }
 
 TabContent.prototype.updateUI = function() {
-    this.element = DOM.createElement('div', { 'class': 'tabContent' }, this.contentElements);
+    this.element = new Element('div', { 'class': 'tabContent' });
+    for (var i = 0; i < this.contentElements.length; ++i) {
+        this.element.insert(this.contentElements[i]);
+    }
 }
 
 TabContent.prototype.setSelected = function(selected) {
     this.selected = selected;
-    DOM.setHasClass(this.element, 'selected', this.selected);
+    if (selected) {
+        this.element.addClassName('selected');
+    } else {
+        this.element.removeClassName('selected');
+    }
 }
