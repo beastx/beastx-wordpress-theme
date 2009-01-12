d2b53d61a2ec423fafadeb2d40d7dfdc29ccfe45
Leandro Asrilevich <beastx@gmail.com>
2009-01-12 06:46:16 -0200

    added some data in post info template

--- postInfo.php
+++ postInfo.php
@@ -1,8 +1,19 @@
 <div class="postinfo"> 
-    <? if ($postCount==1) { social_bookmarks(); }?>
-    <div class="postInCategories">Post in <? the_category(', ') ?> </div>
-    <div class="commentslink">
-        <?php comments_template(); ?>
+    <? if ($postCount==1 || is_single()) { social_bookmarks(); }?>
+    <div class="postInCategories">Posted in <? the_category(', ') ?> </div>
+    <? if ($postCount==1 || is_single()) { if (function_exists('wp_print')) { print_link(); }} ?> 
+    <span id="rss">
+        Subscribe to 
+        <a href="<? bloginfo('rss2_url'); ?>" title="<? _e('Subscribe to RSS feed'); ?>" rel="nofollow">
+            <? _e('<abbr title="Subscribe to RSS Feed">RSS</abbr>'); ?>
+        </a>
+        feed
+    </span>
+    <? edit_post_link('Edit', ' | ', ''); ?>
+    <? if ( (function_exists('similar_posts')) && is_single()) { /* Related Posts Plugin */ ?> 
+        <div class="relpost" ><? similar_posts(); ?></div>
+    <? } ?>
+    <div class="comments_block">
+        <? comments_template(); ?>
     </div>  
-    <? if ($postCount==1) { if(function_exists('wp_print')) { print_link(); }} ?> 
 </div>
