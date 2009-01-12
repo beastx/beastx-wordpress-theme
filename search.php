c282f1ac6f0f2b7cafa76778ee07f56b74892d98
Leandro Asrilevich <beastx@gmail.com>
2009-01-12 04:57:20 -0200

    added archive page search and single pages (incomplete yet)

---
+++ search.php
@@ -0,0 +1,10 @@
+<? get_header(); ?>
+<div class="rightSideBar">
+    <? get_sidebar(); ?>
+</div>
+<div class="contentContainer">
+    <div id="content">
+        search Page
+    </div>
+</div>
+<? get_footer(); ?>
