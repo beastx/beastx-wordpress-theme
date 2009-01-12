aee5e304bffb12ed4222d5294bd4fe4302f5f69e
Leandro Asrilevich <beastx@gmail.com>
2009-01-12 04:54:10 -0200

    added 404 file not found page (incomplete yet)

---
+++ 404.php
@@ -0,0 +1,10 @@
+<? get_header(); ?>
+<div class="rightSideBar">
+    <? get_sidebar(); ?>
+</div>
+<div class="contentContainer">
+    <div id="content">
+        404 page
+    </div>
+</div>
+<? get_footer(); ?>
