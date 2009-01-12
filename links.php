fa0a925f035fbf9a931f047be225cc1d64aa9254
Leandro Asrilevich <beastx@gmail.com>
2009-01-12 05:17:40 -0200

    added links template file

---
+++ links.php
@@ -0,0 +1,13 @@
+<? get_header(); ?>
+<div class="rightSideBar">
+    <? get_sidebar(); ?>
+</div>
+<div class="contentContainer">
+    <div id="content">
+        <h2>Links:</h2>
+        <ul>
+            <?php get_links_list(); ?>
+        </ul>
+    </div>
+</div>
+<? get_footer(); ?>
